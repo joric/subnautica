d853430d2b0e2a0c91784741cb634e3082130590
joric <joric@users.noreply.github.com>
2026-06-07 05:39:01 +0500

    updated files

--- scripts/make_regions.cmd
+++ scripts/make_regions.cmd
@@ -1,5 +1,8 @@
 rem npm install -g mapshaper
 
 python make_regions.py
-mapshaper regions.json -simplify dp 10%% -clean -explode -o ../data/regions.json
+
+rem use Visvalingam / weighted area (by default) simplification to 10%
+
+mapshaper regions.json -simplify 10%% -clean -explode -o ../data/regions.json
 
